ca94cf16a59900b85ebec593c683ddd045343404
MathieuDacheux <mathieu.dacheux@icloud.com>
2022-10-27 16:17:20 +0200

    Push

--- views/admin/clientProfil.php
+++ views/admin/clientProfil.php
@@ -2,15 +2,17 @@
     <div class="containerRecap">
         <div class="containerProfil">
             <div class="containerTitle flexCenterCenter">
-                <h3 class="titleColor">Client</h3>
+                <h3 class="titleColor">Information du client</h3>
             </div>
             <div class="containerContent">
-                <p>
-                    Dacheux
-                </p>
-                <p>
-                    Mathieu
-                </p>
+                <div class="containerName">
+                    <p>
+                        Mathieu
+                    </p>
+                    <p>
+                        Dacheux
+                    </p>
+                </div>
                 <p>
                     mathieu.dacheux@icloud.com
                 </p>
@@ -25,11 +27,23 @@
                 </p>
             </div>
             <div class="containerButton flexCenterCenter">
-                <a href="/dashboard/client?id=">Modifier le client</a>
+                <a href="/dashboard/profil-client?id=">Modifier le client</a>
             </div>
         </div>
         <div class="containerForm">
+            <div class="containerTitle flexCenterCenter">
+                <h3 class="titleColor">Facture du client</h3>
+            </div>
+            <div class="formContent">
+                <div class="containerBills">
 
+                </div>
+                <form method="POST" enctype="multipart/form-data" class="flexCenterCenter">
+                    <label for="bills">Déposez une nouvelle facture</label>
+                    <input type="file" name="bills" id="bills">
+                    <button>Envoyer</button>
+                </form>
+            </div>
         </div>
     </div>
 </main>
